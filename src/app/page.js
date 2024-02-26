ae9a8d386fc6c24bde7e91f35d41a289f7d31b92
Chatthong <dynstystrike@gmail.com>
2024-02-26 18:09:34 +0700

    cv

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import Script from "next/script";
+
 export default function Home() {
   return (
     <>
+      <Script src="https://third-party-script.js"></Script>
       <h1>lorem</h1>
     </>
   );
