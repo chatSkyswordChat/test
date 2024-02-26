5822a2aa38185dba290ef80abea2ff86b6faf585
Chatthong <dynstystrike@gmail.com>
2024-02-26 17:47:57 +0700

    vbn

--- src/app/components/step/Step2.js
+++ src/app/components/step/Step2.js
@@ -61,7 +61,7 @@ export default function Step2({ handleNextStep, handlePrevStep }) {
           <small className="text-primary">Validate</small>
         </div>
 
-        <div className="relative max-w-sm" id="datepickerId">
+        {/* <div className="relative max-w-sm" id="datepickerId">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
               className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -80,7 +80,7 @@ export default function Step2({ handleNextStep, handlePrevStep }) {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Select date"
           />
-        </div>
+        </div> */}
 
         <div>
           <label>วันเดือนปีเกิด</label>
