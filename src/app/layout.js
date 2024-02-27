1d9073fe4f261e022197db0f34bd70aa24c9741a
Chatthong <dynstystrike@gmail.com>
2024-02-27 09:49:06 +0700

    fgh

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-// import NavbarMenuJapanAllPass from "@/app/components/navbar-menu-japan-all-pass/NavbarMenuJapanAllPass";
+import NavbarMenuJapanAllPass from "@/app/components/navbar-menu-japan-all-pass/NavbarMenuJapanAllPass";
 import FooterJapanAllPass from "@/app/components/footer-japan-all-pass/FooterJapanAllPass";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* <NavbarMenuJapanAllPass /> */}
+        <NavbarMenuJapanAllPass />
         <main className="container mx-auto px-4 my-8 md:my-16">{children}</main>
         <FooterJapanAllPass />
 
